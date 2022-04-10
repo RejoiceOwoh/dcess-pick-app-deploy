8b0e1c907f5bdbeffabdf070928d111983ae6bef
Owoh Rejoice Chidera <owohrejoice@gmail.com>
2022-04-10 20:35:44 +0100

    Installattions till use effect done, tested and approved

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
